a44e8d9a4cb785a4b253fcc079a6f62e5c0e8adc
taehwan011 <taehwan011@gmail.com>
2024-06-03 20:47:58 +0900

    장바구니

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-const addToCartButton = document.querySelectorAll(".add-Cart-Button");
-
-addToCartButton.addEventListener("click", () =>{
-    const productName = button.getAttribute("data-name");
-    const productPrice = button.getAttribute("data-price");
-    const productImage = button.getAttribute("data-image");
-    addToCart(productName, productPrice, productImage);
-})
-
-function addToCart(name, price){
-
-}
+class Products {
+    async getProducts(){
+    try {
+     let result = await fetch("products.json");
+     let data = await result.json();
+ 
+     let products = data.items;
+     products = products.map(item =>{
+         const {title,price} = item.fields;
+         const {id} = item.sys;
+         const image = item.fields.image.fields.file.url;
+         return {title,price,id,image};
+     })
+     return products;
+    } catch (error) {
+        console.log(error);
+     }
+   }
+ }
